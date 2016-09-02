5646487f8e14d9d966e024af11bb979260de66f3
Roilan <roilan14@gmail.com>
2016-09-02 07:46:29 -0400

    working action and reducer test with examples

---
+++ examples/__tests__/reducer-test.js
@@ -0,0 +1,28 @@
+const reducerTest = require('../../index').reducerTest;
+
+const SET_NAME = 'SET_NAME';
+
+const setName = (name) => ({
+  type: SET_NAME,
+  name
+});
+
+function reducer(state, action) {
+  switch (action.type) {
+    case SET_NAME:
+      return {
+        name: action.name
+      };
+
+    default:
+      return state;
+  }
+}
+
+reducerTest(
+  'should change your name and return a new state object',
+  reducer,
+  { name: 'foo' },
+  setName('bar'),
+  { name: 'bar' }
+);
